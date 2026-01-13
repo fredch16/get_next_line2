5f4628777e2f83f9799a0ff96c13995b2557cfea
Frederick Charbonnier <frederickcharbonnier@icloud.com>
2026-01-13 18:00:39 +0100

    Norm conform but failing paco -s on null check where files have text

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,11 @@
 /*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 15:44:09 by fredchar          #+#    #+#             */
-/*   Updated: 2026/01/13 16:34:43 by fredchar         ###   ########.fr       */
+/*   Updated: 2026/01/13 18:00:18 by fredchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-// "12345\n678" len = 9 i = 6
 
 int	ft_strlen(char *str)
 {
@@ -26,50 +24,41 @@ int	ft_strlen(char *str)
 
 char	*extract_line(char **stash)
 {
-	char	*oldstash;
-	int		i;
 	char	*line;
-	char	*newstash;
-	int		stash_len;
-	int		new_len;
+	char	*temp;
+	int		i;
 
 	i = 0;
-	oldstash = *stash;
-	while (oldstash[i] && oldstash[i] != '\n')
+	while ((*stash)[i] && (*stash)[i] != '\n')
 		i++;
-	line = (char *)malloc(sizeof(char) * (i + 2));
-	ft_strlcpy(line, oldstash, i + 2);
-	stash_len = ft_strlen(oldstash);
-	new_len = stash_len - (i + 1);
-	if (new_len <= 0)
-	{
-		newstash = (char *)malloc(1);
-		newstash[0] = '\0';
-	}
-	else
-	{
-		newstash = (char *)malloc(sizeof(char) * (new_len + 1));
-		ft_strlcpy(newstash, (oldstash + i + 1), new_len + 1);
-	}
-	free(oldstash);
-	*stash = newstash;
+	if ((*stash)[i] == '\n')
+		i++;
+	line = malloc(i + 1);
+	if (!line)
+		return (NULL);
+	ft_strlcpy(line, *stash, i + 1);
+	temp = ft_strdup(*stash + i);
+	if (!temp)
+		return (NULL);
+	free(*stash);
+	*stash = temp;
 	return (line);
 }
 
-void	stash_init(char **stash_pointer)
-{
-	char	*stash;
+// void stash_init(char **stash_pointer)
+// {
+// 	char *stash;
 
-	stash = *stash_pointer;
-	if (!stash)
-	{
-		stash = (char *)malloc(1);
-		if (!stash)
-			return ;
-		stash[0] = '\0';
-		*stash_pointer = stash;
-	}
-}
+// 	stash = *stash_pointer;
+// 	if (!stash)
+// 	{
+// 		stash = (char *)malloc(1);
+// 		if (!stash)
+// 			return;
+// 		stash[0] = '\0';
+// 		*stash_pointer = stash;
+// 	}
+// }
 
 char	*stash_update(char *stash, char *buffer)
 {
@@ -82,66 +71,32 @@ char	*stash_update(char *stash, char *buffer)
 
 char	*get_next_line(int fd)
 {
-	char		*buffer;
 	static char	*stash;
+	char		*buffer;
 	int			bytes;
 
-	bytes = 0;
-	if (BUFFER_SIZE <= 0 || fd < 0 || fd >= FOPEN_MAX)
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd >= FOPEN_MAX)
 		return (NULL);
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
-		return (NULL);
-	stash_init(&stash);
-	while (!(ft_strchr(stash, '\n')))
+	if (!stash)
+		stash = ft_strdup("");
+	buffer = malloc((size_t)BUFFER_SIZE + 1);
+	if (!buffer || !stash)
+		return (free(buffer), free(stash), stash = NULL, NULL);
+	bytes = 1;
+	while (bytes > 0 && !ft_strchr(stash, '\n'))
 	{
 		bytes = read(fd, buffer, BUFFER_SIZE);
-		if (bytes <= 0)
-			break ;
-		buffer[bytes] = '\0';
-		stash = stash_update(stash, buffer);
+		if (bytes > 0)
+		{
+			buffer[bytes] = '\0';
+			stash = stash_update(stash, buffer);
+		}
 	}
 	free(buffer);
-	if (bytes == -1 || !stash || !stash[0])
-	{
-		free(stash);
-		stash = NULL;
-		return (NULL);
-	}
+	if (bytes == -1 || !stash || !*stash)
+		return (free(stash), stash = NULL, NULL);
 	return (extract_line(&stash));
 }
-
-// char	*get_next_line(int fd)
-// {
-// 	char		buffer[BUFFER_SIZE + 1];
-// 	static char	*stash;
-// 	int			bytes;
-
-// 	if (BUFFER_SIZE <= 0 || fd < 0 || fd >= FOPEN_MAX)
-// 		return (NULL);
-// 	stash_init(&stash);
-// 	while (!(ft_strchr(stash, '\n')))
-// 	{
-// 		bytes = read(fd, buffer, BUFFER_SIZE);
-// 		if (bytes <= 0)
-// 			break ;
-// 		if (bytes == -1)
-// 		{
-// 			free(stash);
-// 			stash = NULL;
-// 			return (NULL);
-// 		}
-// 		buffer[bytes] = '\0';
-// 		stash = stash_update(stash, buffer);
-// 	}
-// 	if (!stash[0])
-// 	{
-// 		free(stash);
-// 		stash = NULL;
-// 		return (NULL);
-// 	}
-// 	return (extract_line(&stash));
-// }
 
 // int main(int argc, char **argv)
 // {
