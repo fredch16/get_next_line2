8e8c56e4cad5c5522f988616eec5256a94a4c62b
Frederick Charbonnier <frederickcharbonnier@icloud.com>
2026-01-13 16:35:37 +0100

    Changed LOADS. Turns out buffersize being huge caused problems given that my buffer is neither static nor allocated and then causes a stack overflow. So I've fixed that. Valgrind looks clean now all thats left is to norm it once again

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 15:44:09 by fredchar          #+#    #+#             */
-/*   Updated: 2026/01/12 17:34:47 by fredchar         ###   ########.fr       */
+/*   Updated: 2026/01/13 16:34:43 by fredchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,26 @@ char	*extract_line(char **stash)
 	char	*line;
 	char	*newstash;
 	int		stash_len;
+	int		new_len;
 
 	i = 0;
 	oldstash = *stash;
 	while (oldstash[i] && oldstash[i] != '\n')
 		i++;
-	line = (char *)malloc(sizeof(char) * i + 2);
+	line = (char *)malloc(sizeof(char) * (i + 2));
 	ft_strlcpy(line, oldstash, i + 2);
 	stash_len = ft_strlen(oldstash);
-	newstash = (char *)malloc(sizeof(char) * (stash_len - (i + 1)));
-	ft_strlcpy(newstash, (oldstash + i + 1), stash_len - i);
+	new_len = stash_len - (i + 1);
+	if (new_len <= 0)
+	{
+		newstash = (char *)malloc(1);
+		newstash[0] = '\0';
+	}
+	else
+	{
+		newstash = (char *)malloc(sizeof(char) * (new_len + 1));
+		ft_strlcpy(newstash, (oldstash + i + 1), new_len + 1);
+	}
 	free(oldstash);
 	*stash = newstash;
 	return (line);
@@ -53,7 +63,7 @@ void	stash_init(char **stash_pointer)
 	stash = *stash_pointer;
 	if (!stash)
 	{
-		stash = malloc(1);
+		stash = (char *)malloc(1);
 		if (!stash)
 			return ;
 		stash[0] = '\0';
@@ -72,31 +82,66 @@ char	*stash_update(char *stash, char *buffer)
 
 char	*get_next_line(int fd)
 {
-	char		buffer[BUFFER_SIZE + 1];
+	char		*buffer;
 	static char	*stash;
 	int			bytes;
 
+	bytes = 0;
 	if (BUFFER_SIZE <= 0 || fd < 0 || fd >= FOPEN_MAX)
+		return (NULL);
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
 		return (NULL);
 	stash_init(&stash);
 	while (!(ft_strchr(stash, '\n')))
 	{
 		bytes = read(fd, buffer, BUFFER_SIZE);
-		if (bytes == 0)
+		if (bytes <= 0)
 			break ;
-		if (bytes == -1)
-		{
-			free(stash);
-			stash = NULL;
-			return (NULL);
-		}
 		buffer[bytes] = '\0';
 		stash = stash_update(stash, buffer);
 	}
-	if (!stash[0])
+	free(buffer);
+	if (bytes == -1 || !stash || !stash[0])
+	{
+		free(stash);
+		stash = NULL;
 		return (NULL);
+	}
 	return (extract_line(&stash));
 }
+
+// char	*get_next_line(int fd)
+// {
+// 	char		buffer[BUFFER_SIZE + 1];
+// 	static char	*stash;
+// 	int			bytes;
+
+// 	if (BUFFER_SIZE <= 0 || fd < 0 || fd >= FOPEN_MAX)
+// 		return (NULL);
+// 	stash_init(&stash);
+// 	while (!(ft_strchr(stash, '\n')))
+// 	{
+// 		bytes = read(fd, buffer, BUFFER_SIZE);
+// 		if (bytes <= 0)
+// 			break ;
+// 		if (bytes == -1)
+// 		{
+// 			free(stash);
+// 			stash = NULL;
+// 			return (NULL);
+// 		}
+// 		buffer[bytes] = '\0';
+// 		stash = stash_update(stash, buffer);
+// 	}
+// 	if (!stash[0])
+// 	{
+// 		free(stash);
+// 		stash = NULL;
+// 		return (NULL);
+// 	}
+// 	return (extract_line(&stash));
+// }
 
 // int main(int argc, char **argv)
 // {
@@ -121,6 +166,13 @@ char	*get_next_line(int fd)
 //         printf("%s", line);
 //         free(line);
 //     }
+// 	// line = get_next_line(fd);
+// 	// printf("1: %s", line);
+// 	// free(line);
+
+// 	// line = get_next_line(fd);
+// 	// printf("2: %s", line);
+// 	// free(line);
 
 //     close(fd);
 //     return (0);
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,15 @@
 /*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 15:34:00 by fredchar          #+#    #+#             */
-/*   Updated: 2026/01/12 17:34:13 by fredchar         ###   ########.fr       */
+/*   Updated: 2026/01/13 15:57:40 by fredchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 4
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000000
+# endif
 
 # include <stdio.h>
 # include <fcntl.h>
@@ -30,5 +32,6 @@ char			*ft_strjoin(char const *s1, char const *s2);
 // get_next_line.c
 
 int				ft_strlen(char *str);
+char			*get_next_line(int fd);
 
 #endif
